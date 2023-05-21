3be3b101c0b45db457fab8a63785e9fb557d6815
muranakar <p1211065@gmail.com>
2023-05-21 21:12:56 +0900

    Initial Commit

---
+++ MVVMPattern1/MVVMPattern1App.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMPattern1App.swift
+//  MVVMPattern1
+//
+//  Created by 村中令 on 2023/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVMPattern1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
